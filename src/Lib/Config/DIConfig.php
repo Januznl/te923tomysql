54bd6cd5cd5a60d8817527d21924029912bdb400
Jan Walraven <info@beyonditsolutions.nl>
2015-09-29 22:30:11 +0200

    Config now dynamically loaded when initiating entityManager

--- src/Lib/Config/DIConfig.php
+++ src/Lib/Config/DIConfig.php
@@ -9,18 +9,14 @@ class DIConfig extends Config
 {
     public function define(Container $di)
     {
+
         //Define Config
         $di->set('config.settings', $di->lazyNew('TE923ToMysql\Lib\Config\Config'));
         $di->params['TE923ToMysql\Lib\Config\Config']['configParser'] = $di->lazyNew('Symfony\Component\Yaml\Yaml');
 
-//        //define doctrine config
-//        $di->set('config.doctrine', $di->lazyNew('TE923ToMysql\Lib\Config\Doctrine'), array(
-//            $di->lazyGet('config.settings')
-//        ));
+        //Define paramaters for Doctrine config class
+        $di->params['TE923ToMysql\Lib\Config\Doctrine']['config'] = $di->lazyGet('config.settings');
 
-        $di->params['TE923ToMysql\Lib\Config\Doctrine'] = [
-            'config' => $di->lazyGet('config.settings')
-        ];
 
         //define doctrine cache
         $di->set('doctrine.cache', $di->lazy(function () use ($di) {
@@ -33,13 +29,13 @@ class DIConfig extends Config
             }
         }));
 
-        //Setup doctrine entity manager
+        //Setup doctrine entity manager (return static create value)
         $di->set('doctrine.entityManager', $di->lazy(array('Doctrine\ORM\EntityManager', 'create'),$di->lazyGet('doctrine.connectionOption') ,$di->lazyGet('doctrine.entityConfig')));
 
         $di->set('doctrine.connectionOption', $di->lazy(function () use ($di) {
             /** @var \TE923ToMysql\Lib\Config\Doctrine $doctrineConfig */
             $doctrineConfig = $di->newInstance('TE923ToMysql\Lib\Config\Doctrine');
-            var_dump($doctrineConfig->getHost());
+
             return array(
                     'driver'    => $doctrineConfig->getDriver(),
                     'host'      => $doctrineConfig->getHost(),
